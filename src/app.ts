1190c97640711460b33267e0efb55ec0fcb986a8
Evgeny Gendel <evhandel@gmail.com>
2025-05-27 16:28:24 +0400

    increase request limit

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 app.use(cors());
 
 // Error handling middleware
